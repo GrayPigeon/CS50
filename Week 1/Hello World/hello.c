752b96bf409306b4bde4b338803829e979c11ff6
Nick Pfeiff <67116440+GrayPigeon@users.noreply.github.com>
2020-07-17 14:01:27 -0400

    Update hello.c

--- Week 1/Hello World/hello.c
+++ Week 1/Hello World/hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <cs50.h>
 
-
-int main(void) {
+// Function that prints inquires the user's name, and returns the string: "Hello, [name]!"
+int main(void) 
+{
+    // Inquire name
     string name = get_string("What is your name!\n");
+    
+    // Say hi
     printf("Hello, %s!\n", name);
 }
